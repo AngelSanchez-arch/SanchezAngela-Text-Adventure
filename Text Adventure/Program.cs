8d6958f2d34dbaa37e0656c51f100abc1e9b3524
MUKILTEO\1720986 <Kirby336242@gmail.com>
2024-10-03 13:25:59 -0700

    hi

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Text_Adventure
+﻿using System.ComponentModel.Design;
+using System.Numerics;
+
+namespace Text_Adventure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            // /r means to go back some rooms
+            Console.WriteLine("Hello, To Find your crush!");
+            Console.WriteLine("I'm your host, Viola");
+            Console.WriteLine("We Have a very special Guest. The amazing, Incredabile, Joe");
+            Console.WriteLine("You'll pick between 3 Contestants");
+            Console.WriteLine("Amelia | Charlet | Bass | Etterberg");
+            //to upper uppercases all lettters
+            string input = Console.ReadLine();
+            if (input.ToUpper() == "Amelia")
+            {
+                Console.WriteLine("Amelia, Please stand up");
+            }
+            //Each one
+            else if (input.ToUpper() == "Charlet")
+            {
+                Console.WriteLine("Charlet, Please stand up");
+
+            }
+            else if (input == "Bass")
+            {
+                Console.WriteLine("Bass, Please stand up");
+            }
+            else if (input.ToUpper() == "Etterberg") 
+            {
+                Console.WriteLine("Etterberg, Please stand up");
+                Console.WriteLine("Etterberg, Introduce yourself");
+                Console.WriteLine("hi im etterberger (yes the misspelling is intentional lol)");
+                Console.WriteLine("ummmmmmmmmmmmmmmmmmmmmmmmmmmm. Im drawing a blAnk hEre.");
+            }
         }
+
+        //static void 
+        // void is a funtion
+        //Exicutes lines of code top to bottom
     }
 }
