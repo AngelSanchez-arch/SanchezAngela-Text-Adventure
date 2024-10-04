dc2bd518049ee87316d0a335c31a4828baab0b23
MUKILTEO\1720986 <Kirby336242@gmail.com>
2024-10-04 13:27:37 -0700

    hi

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -13,7 +13,7 @@ namespace Text_Adventure
             Console.WriteLine("I'm your host, Viola");
             Console.WriteLine("We Have a very special Guest. The amazing, Incredabile, Joe");
             Console.WriteLine("You'll pick between 3 Contestants");
-            Console.WriteLine("Amelia | Charlet | Bass | Etterberg");
+            Console.WriteLine("Amelia  | Bass ");
             //to upper uppercases all lettters
             string input = Console.ReadLine();
             if (input.ToUpper() == "Amelia")
@@ -21,21 +21,9 @@ namespace Text_Adventure
                 Console.WriteLine("Amelia, Please stand up");
             }
             //Each one
-            else if (input.ToUpper() == "Charlet")
-            {
-                Console.WriteLine("Charlet, Please stand up");
-
-            }
             else if (input == "Bass")
             {
                 Console.WriteLine("Bass, Please stand up");
-            }
-            else if (input.ToUpper() == "Etterberg") 
-            {
-                Console.WriteLine("Etterberg, Please stand up");
-                Console.WriteLine("Etterberg, Introduce yourself");
-                Console.WriteLine("hi im etterberger (yes the misspelling is intentional lol)");
-                Console.WriteLine("ummmmmmmmmmmmmmmmmmmmmmmmmmmm. Im drawing a blAnk hEre.");
             }
         }
 
